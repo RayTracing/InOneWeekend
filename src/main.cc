579addf2d1fd0d9244b780ec1f607535e9d3a3bc
Ronald Wotzlaw <wotzlaw@gmail.com>
2019-08-17 07:32:16 +0200

    Add multithreading using std::thread
    The rendered image is divided into number of threads many pieces along the rows of the image. Each piece then is rendered in its own thread.
    
    Tested on my iMac with 4 cores / 8 hyperthreads.
    
    Timing before change: 58s
    Timing after change: 18s

--- src/main.cc
+++ src/main.cc
@@ -9,7 +9,9 @@
 // with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 //==================================================================================================
 
+#include <functional>
 #include <iostream>
+#include <thread>
 #include <vector>
 #include "sphere.h"
 #include "hitable_list.h"
@@ -104,22 +106,31 @@ int main() {
     camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture, dist_to_focus);
 
     std::vector<uint8_t> image(nx * ny * 3);
-    for (int j = ny-1; j >= 0; j--) {
-        for (int i = 0; i < nx; i++) {
-            vec3 col(0, 0, 0);
-            for (int s=0; s < ns; s++) {
-                float u = float(i + drand48()) / float(nx);
-                float v = float(j + drand48()) / float(ny);
-                ray r = cam.get_ray(u, v);
-                col += color(r, world,0);
+    const int k_threads = std::thread::hardware_concurrency();
+    std::vector<std::thread> threads(k_threads);
+    for (int t = 0; t < k_threads; ++t) {
+        threads[t] = std::thread(std::bind([&](int start, int end, int t) {
+            for (int j = start; j < end; j++) {
+                for (int i = 0; i < nx; i++) {
+                    vec3 col(0, 0, 0);
+                    for (int s=0; s < ns; s++) {
+                        float u = float(i + drand48()) / float(nx);
+                        float v = float(j + drand48()) / float(ny);
+                        ray r = cam.get_ray(u, v);
+                        col += color(r, world,0);
+                    }
+                    col /= float(ns);
+                    col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
+                  
+                    image[3*(j*nx+i)+0] = int(255.99*col[0]);
+                    image[3*(j*nx+i)+1] = int(255.99*col[1]);
+                    image[3*(j*nx+i)+2] = int(255.99*col[2]);
+                }
             }
-            col /= float(ns);
-            col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
-       
-            image[3*(j*nx+i)+0] = int(255.99*col[0]);
-            image[3*(j*nx+i)+1] = int(255.99*col[1]);
-            image[3*(j*nx+i)+2] = int(255.99*col[2]);
-        }
+        }, t*ny/k_threads, (t+1)==k_threads ? ny : (t+1)*ny/k_threads, t));
+    }
+    for (int t = 0; t < k_threads; ++t) {
+        threads[t].join();
     }
     write_image(&image[0], nx, ny);
 }
