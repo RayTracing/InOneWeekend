84b1338fbcaf9fce091578a32714d42804339ad8
vassillenchizhov@gmail.com <vassillenchizhov@gmail.com>
2019-08-23 21:55:32 +0200

    Addresses issue: #58
    Adds very basic multi-threading with OpenMP. Requires /openmp command line option in MSVC.
    
    Also saves the image to a temporary buffer before writing to disk and adds a clock to count the rendering time.

--- src/main.cc
+++ src/main.cc
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <limits>				// std::numeric_limits<float>::infinity()
 #include <fstream>				// doesn't require output redirection
+#include <chrono>				// Output elapsed time
 #include "sphere.h"
 #include "hitable_list.h"
 #include "float.h"
@@ -95,16 +96,7 @@ int main() {
     int ns = 10;
 
 
-	/*
-		Addresses issue: #18
-		std::ofstream rather than std::cout, so one doesn't need to redirect the output
-	*/
-	// open output file
-	std::ofstream file;
-	const char* outputFilename = "output_image.ppm";
-	file.open(outputFilename);
-	if (!file.good()) std::cerr << "Failed to open file " << outputFilename << std::endl;
-    file << "P3\n" << nx << " " << ny << "\n255\n";
+	
 
 
     hitable *world = random_scene();
@@ -116,7 +108,28 @@ int main() {
 
     camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture, dist_to_focus);
 
-    for (int j = ny-1; j >= 0; j--) {
+	/*
+		Addresses issue: #58
+
+		We write the code to memory before saving to disk, this may prove beneficial not only 
+		with regards to parallelization.
+
+		To use OMP with MSVC add /openmp to the command line options
+	*/
+	vec3* image = new vec3[nx * ny];
+
+
+	/*
+		Extra feature: output rendering time (useful to see the speedup from multi-threading)
+
+		On my machine rendering with 1 core takes: ~8s, and with 4 cores: ~2s
+	*/
+	std::chrono::high_resolution_clock clock;
+	auto timeStamp = clock.now();
+
+
+#pragma omp parallel for
+    for (int j = 0; j < ny ; j++) {
         for (int i = 0; i < nx; i++) {
             vec3 col(0, 0, 0);
             for (int s=0; s < ns; s++) {
@@ -126,16 +139,43 @@ int main() {
                 col += color(r, world,0);
             }
             col /= float(ns);
-            col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
-            int ir = int(255.99*col[0]);
-            int ig = int(255.99*col[1]);
-            int ib = int(255.99*col[2]);
-            file << ir << " " << ig << " " << ib << "\n";
+
+			// flat array indexing, perform the flip here j' = ny-1-j
+			image[i + nx * (ny-1-j)] = col; // store in memory
         }
     }
 
-	//close output file
+	std::chrono::duration<float> elapsedTime = clock.now() - timeStamp;
+	std::cout << "Rendering took: " << elapsedTime.count() << "s.\n";
+
+	/*
+		Addresses issue: #18
+		std::ofstream rather than std::cout, so one doesn't need to redirect the output
+	*/
+	std::ofstream file;
+	const char* outputFilename = "output_image.ppm";
+	file.open(outputFilename);
+	if (!file.good()) std::cerr << "Failed to open file " << outputFilename << std::endl;
+	file << "P3\n" << nx << " " << ny << "\n255\n";
+	for (int i = 0; i < nx * ny; ++i)
+	{
+		vec3 col = image[i];
+
+		// gamma correction
+		col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+
+		// map from [0,1] to {0,1,...,255} (quantization)
+		int ir = int(255.99 * col.r());
+		int ig = int(255.99 * col.g());
+		int ib = int(255.99 * col.b());
+
+		// save pixel to disk
+		file << ir << " " << ig << " " << ib << "\n";
+	}
 	file.close();
+	delete[] image;
+
+	
 
 	return 0;
 }
